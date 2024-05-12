cd9dfbd6aee206241cf9358e3785c8f0fab11fe7
gverafei <143739705+gverafei@users.noreply.github.com>
2024-05-12 11:03:18 -0600

    Add files via upload

---
+++ Data/IDataContext.cs
@@ -0,0 +1,7 @@
+namespace mvc;
+
+public interface IDataContext
+{
+    public Task<List<Producto>> ObtenProductosAsync();
+}
+
---
+++ Models/Producto.cs
@@ -0,0 +1,11 @@
+namespace mvc;
+
+public class Producto
+{
+    public int ProductoId { get; set; }
+    public string? Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public string? Fabricante { get; set; }
+}
+
+
